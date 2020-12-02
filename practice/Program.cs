4a57b9c4376c26bdd4807ce0ddb84a773b062fe4
Pavan Padiyala <PadiyalP@healthshareltd.net>
2020-12-03 11:11:37 +1300

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            string str = "";
+            string reverse= "";
+            int Length;
+
+            Console.WriteLine("Enter a name");
+            str = Console.ReadLine();
+
+            Length = str.Length-1;
+
+            while(Length >=0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+
+            Console.WriteLine("Reverse word is {0}", reverse);
+            Console.ReadLine();
+
+           
+            
+
+
+
+        }
+    }
+}
